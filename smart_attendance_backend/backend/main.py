616084acb5f4c0bcd13d1238bacd895d969003e4
sarthak6157 <sarthakkochar2006@gmail.com>
2026-03-26 19:28:25 +0530

    Update main.py

--- smart_attendance_backend/backend/main.py
+++ smart_attendance_backend/backend/main.py
@@ -11,7 +11,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, # This specifically trusts your portal
+    allow_origins=["https://smart-attendance-portal.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
